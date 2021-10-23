aab6adeb6f63f8ba38217ac747a6bd41c81d437d
DawidKolo <dawid_kolodziej@o2.pl>
2021-10-23 17:45:24 +0200

    bootstrap_added_2

--- ZPI_Theme/header.php
+++ ZPI_Theme/header.php
@@ -2,16 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ZPI_from_scratch</title>
     <?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
-    <p>header paragraph</p>
+    
     <header>
         <section class="top-bar">
-            <div class="social-media-icons">Social Icons</div>
-            <div class="search">search</div>
+            <div class="container">
+                <div class="row">
+                    <div class="social-media-icons col-xl-10 col-md-8 col-sm-8 col-6">Social Icons</div>
+                    <div class="search col-xl-2 col-md-4 col-sm-4 col-6 text-right">search</div>  
+                </div>
+            </div>
+            
         </section>
         <section class="menu-area">
             <section class="logo">Logo</section>
