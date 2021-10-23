63a1dbab9cb7b52f3873ae1b2e45b72ecd02c4b8
DawidKolo <dawid_kolodziej@o2.pl>
2021-10-23 18:11:49 +0200

    menu->functions.php

--- ZPI_Theme/functions.php
+++ ZPI_Theme/functions.php
@@ -7,4 +7,10 @@ function load_scripts(){
     wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+register_nav_menus(
+    array(
+       'my_main_menu' => 'Primary Menu' 
+    )
+);
 ?>
