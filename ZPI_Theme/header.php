a8f07b427908eaf833d65bbe535fc51deab8ab3f
DawidKolo <dawid_kolodziej@o2.pl>
2021-10-23 15:35:39 +0200

    split header_footer.php

---
+++ ZPI_Theme/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>header paragraph</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
+</html>
