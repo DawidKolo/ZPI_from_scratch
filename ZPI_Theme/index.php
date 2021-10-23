2c825534e4fb61dcd0e91f0db607e5d539169032
DawidKolo <dawid_kolodziej@o2.pl>
2021-10-23 15:25:02 +0200

    index_frame_done

--- ZPI_Theme/index.php
+++ ZPI_Theme/index.php
@@ -6,7 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Start of development</title>
 </head>
-<body>
-    
+<body <?php body_class(); ?>>
+    <p>Reference paragraph</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-area">
+                <aside class="sidebar"></aside>
+                <div class="news"></div>
+            </section>    
+            <section class="map"></section>
+        </main>
+    </div>    
+    <footer></footer>
 </body>
 </html>
