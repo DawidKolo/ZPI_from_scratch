2adf576394f00ef86126b989b1b0883efb3a366d
DawidKolo <dawid_kolodziej@o2.pl>
2021-10-25 17:26:24 +0200

    posts loop done

--- ZPI_Theme/index.php
+++ ZPI_Theme/index.php
@@ -23,8 +23,8 @@
 
                         <article>
                             <h2><?php the_title(); ?></h2>
-                            <p>Posted in <?php echo get_the_date() ?>; by <?php the_author_posts_link(); ?></p>
-                            <p>Categories: <?php the_category(''); ?></p>
+                            
+                            <p><?php the_content(); ?></p>
                             
                         </article>
 
